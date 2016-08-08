d9169c8d7120d17521afe8dda85494137f6ee000
Felix Zapata <fzberlinches@gmail.com>
2016-08-08 10:33:21 +0200

    chore(gulpfile.js): add gulpfile for the repository

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+/*
+ * gulp-axe-webdriver
+ * https://github.com/felixzapata/gulp-axe-webdriver
+ *
+ * Copyright (c) 2016 Felix Zapata
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+require('require-dir')('./tasks', {
+  recurse: true
+});
